911829949a991699755a51366bbc71d826dbfe87
JorgeGrandett <j.ivanvillalba@gmail.com>
2022-03-11 10:41:01 -0500

    Base del proyecto

---
+++ src/apirequest/peticiones.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    name: "peticiones-api",
+    created() {
+    axios.get("https://cors-anywhere.herokuapp.com/xkcd.com/info.0.json").then((result) => {
+      console.log(result.data);
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/comic.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <peticiones/>
+    </div>
+</template>
+
+<script>
+import peticiones from '@/apirequest/peticiones.vue'
+export default {
+    name: "comic-vista",
+    components:{
+        peticiones
+    }
+}
+</script>
+
+<style>
+
+</style>
